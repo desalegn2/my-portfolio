a2cb150ec07076de0a9a8281f7b3299806218f5a
desalegn2 <sdesalegn21@gmail.com>
2025-10-02 17:26:45 +0300

    some change

--- src/portfolio/Contact.jsx
+++ src/portfolio/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
                 </div>
                 <div className=''>
                 <p className='text-2xl font-bold inline border-b-4 border-gray-500'>Call Us On</p>
-                    <p className='py-6'>+251949496106</p> 
+                    <p className='py-6'>+251949496106/977241791</p> 
                     <p className='text-2xl font-bold inline border-b-4 border-gray-500'>Email</p>
                     <p className='py-6'>desalegnaw@gmail.com</p>
                     <p className='text-2xl font-bold inline border-b-4 border-gray-500'>Address</p>
